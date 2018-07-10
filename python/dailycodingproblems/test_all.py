ad734a4f98ca61de0eab14c54d688a255f432e57
smhnr27 <25464398+smhnr27@users.noreply.github.com>
2018-07-10 15:50:22 -0400

    Daily Problem 2

--- python/dailycodingproblems/test_all.py
+++ python/dailycodingproblems/test_all.py
@@ -1,5 +1,6 @@
 import unittest
 import problem1
+import problem2
 
 class TestProbs(unittest.TestCase):
 
@@ -12,6 +13,10 @@ class TestProbs(unittest.TestCase):
 		self.assertEqual(problem1.addCheck([10, -4, 7, -1], -1), False)
 		self.assertEqual(problem1.addCheck([-1, 0, 2], -2), False)
 		self.assertEqual(problem1.addCheck([], -2), False)
+
+	def test_multArray(self):
+		self.assertEqual(problem2.multArray([1, 2, 3, 4, 5]), [120, 60, 40, 30, 24])
+		self.assertEqual(problem2.multArray([3, 2, 1]), [2, 3, 6])
 
 if __name__ == '__main__':
 	unittest.main()
