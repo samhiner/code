d78cface0ac185e248dce54ef87b625c34061583
smhnr27 <25464398+smhnr27@users.noreply.github.com>
2018-07-12 17:54:14 -0400

    Daily Problem 4

--- python/dailycodingproblems/test_all.py
+++ python/dailycodingproblems/test_all.py
@@ -1,11 +1,11 @@
 import unittest
 import problem1
 import problem2
+import problem4
 
 class TestProbs(unittest.TestCase):
 
-	#problem1.py
-	def test_addCheck(self):
+	def test_problem1(self):
 		self.assertEqual(problem1.addCheck([10, 3, 7], 10), True)
 		self.assertEqual(problem1.addCheck([2, 5, 1, 1, 1, 5], 0), False)
 		self.assertEqual(problem1.addCheck([10, -4, 3, -100], -1), True)
@@ -14,9 +14,20 @@ class TestProbs(unittest.TestCase):
 		self.assertEqual(problem1.addCheck([-1, 0, 2], -2), False)
 		self.assertEqual(problem1.addCheck([], -2), False)
 
-	def test_multArray(self):
+	def test_problem2(self):
 		self.assertEqual(problem2.multArray([1, 2, 3, 4, 5]), [120, 60, 40, 30, 24])
 		self.assertEqual(problem2.multArray([3, 2, 1]), [2, 3, 6])
+
+	def test_problem4(self):
+		self.assertEqual(problem4.lowestAbsentInt([3, 4, -1, 1]), 2)
+		self.assertEqual(problem4.lowestAbsentInt([1, 2, 0]), 3)
+
+'''
+Template Unit Test:
+
+def test_(self):
+	self.assertEqual(problemX.function(input),output)
+'''
 
 if __name__ == '__main__':
 	unittest.main()
