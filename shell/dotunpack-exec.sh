041b099b38836ee6bb5af88c03e4421b13656bef
Sam Hiner <25464398+samhiner@users.noreply.github.com>
2018-10-27 20:11:45 -0400

    Add structure for code string replacer.
    This doesn't work, just moving from school comp to home one. It is basically find and replace that runs on commit for production code so you don't have to open-source your passwords

---
+++ shell/dotunpack-exec.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+config=$(cat $1)
+
+curr=NONE
+files=()
+declare -a repl_words #TODO the "a" will have to be capital for non-bash shell
+IFS=$'\n'
+for line in $config
+do
+	echo $curr
+	echo $line + 'd'
+
+
+	if [ $curr == FILES ]
+	then
+		files+=("$line")
+		continue
+	elif [ $curr == VARS ]
+	then
+		IFS="=" read old_word new_word <<< $line #TODO separate into many lines
+		echo "test"
+		echo $old_word
+		echo $new_word
+		repl_words[$old_word]=$new_word #TODO make this support slashes
+		continue
+	fi
+
+	if [ "$line" == FILES ]
+	then
+		curr=FILES
+	elif [ "$line" == VARS ]
+	then
+		curr=VARS
+	fi
+done
+: '
+for i in "${!repl_words[@]}"
+do
+  echo "key  : $i"
+  echo "value: ${repl_words[$i]}"
+done'
+
+echo "${repl_words[localhost]}"
+
+echo $files
