48c99d8fb8a3cadd3c1da96e9ce0916ed92fcae1
samhiner <25464398+samhiner@users.noreply.github.com>
2019-02-03 17:09:01 -0500

    Basic attendance checking complete.

--- python/ncsenate-attendance/getsenatortables.py
+++ python/ncsenate-attendance/getsenatortables.py
@@ -2,7 +2,8 @@ from bs4 import BeautifulSoup
 import requests
 import csv
 
-def get_vote_csv(senator_id, year):
+#turn online records into csv
+def download_voting_record(senator_id, year):
 	#find html table
 	url = 'https://www.ncleg.gov/Legislation/Votes/MemberVoteHistory/' + str(year) + '/S/' + str(senator_id)
 	html = requests.get(url).content
@@ -10,30 +11,32 @@ def get_vote_csv(senator_id, year):
 	table = soup.select_one('table#vote-report')
 
 	#if page actually doesn't exist, redirect to page that is always there or throw error
-	if table == None && year != 2019:
-		return get_vote_csv(2019)
-	else if table == None && year == 2019:
+	if table == None and year != 2019:
+		return download_voting_record(senator_id, 2019)
+	elif table == None and year == 2019:
 		raise ValueError('Senator ID out of bounds.')
 
-	#
-	headers = [th.text.encode('utf-8') for th in table.select('tr th')]
-
+	#write table to csv file
 	with open('data/' + senator_id + '.csv', 'w') as file:
 		writer = csv.writer(file)
+		headers = [th.text.encode('utf-8') for th in table.select('tr th')]
 		writer.writerow(headers)
 		writer.writerows([[td.text.encode('utf-8') for td in row.find_all('td')] for row in table.select('tr + tr')])
 
-def download_voting_records(senator_list):
+	print('Senator ' + senator_id + ' successfully downloaded for year ' + str(year) + '!')
+
+
+def get_senator_records(senator_list):
 	for senator in senator_list:
 		#get ID of senator to be used to find vote records
 		senator_id = senator[15:18]
 		if senator_id[-1] == '"':
 			senator_id = senator_id[:-1]
 
-		get_vote_csv(senator_id, 2017)
+		download_voting_record(senator_id, 2017)
 
 
 with open('data/senatorlist.txt') as file:
-	senator_list = file.split('\n')
+	senator_list = file.read().split('\n')
 
-download_voting_records(senator_list)
+get_senator_records(senator_list)
